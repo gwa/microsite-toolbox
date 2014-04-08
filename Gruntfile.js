1afc879fdbacedd62fe19fc2737978869493dabb
Timothy Groves <groves@greatwhiteark.com>
2014-04-08 14:49:09 +0200

    Added productboxes css

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,21 @@ module.exports = function(grunt) {
 		// tasks
 		jscs: grunt.file.readJSON('./grunt/tasks/jscs.json'),
 		jshint: grunt.file.readJSON('./grunt/tasks/jshint.json'),
-		jasmine: grunt.file.readJSON('./grunt/tasks/jasmine.json')
+		jasmine: grunt.file.readJSON('./grunt/tasks/jasmine.json'),
+		csslint: grunt.file.readJSON('./grunt/tasks/csslint.json')
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-jscs-checker');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 
 	grunt.registerTask(
 		'default',
 		[
 			'jscs',
 			'jshint:src',
+			//'csslint',
 			'jasmine'
 		]
 	);
